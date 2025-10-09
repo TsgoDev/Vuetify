e0f8a220b6058c7045732e12d3009e4ab1c7eede
Tiago Soares <tsgo.soares@gmail.com>
2025-10-09 09:53:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <!-- Navbar -->
+    <v-app-bar flat class="border-b">
+      <v-app-bar-title>Meu app</v-app-bar-title>
+    </v-app-bar>
+
+    <!-- Main -->
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+//
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-main>
+    <v-container>
+      <h1>Dashboard</h1>
+      <v-row>
+        <v-col cols="12" md="4" lg="3">
+          <!---CARD 1-->
+          <v-card flat class="border">
+            <v-img class="align-end text-white"
+              src="https://images.pexels.com/photos/3700900/pexels-photo-3700900.jpeg">
+              <v-card-title>Top 10 Praias da Bahia</v-card-title>
+            </v-img>
+            <v-card-subtitle class="pt-3">Salvador</v-card-subtitle>
+            <v-card-text>
+              <div>Rio vermelho</div>
+
+              <div>Lorem, ipsum dolor sit</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn>Ver mais</v-btn>
+            </v-card-actions>
+          </v-card>
+
+          <!---CARD 2-->
+        </v-col>
+        <v-col cols="12" md="4" lg="3">
+          <v-card flat class="border">
+            <v-img class="align-end text-white"
+              src="https://images.pexels.com/photos/3700900/pexels-photo-3700900.jpeg">
+              <v-card-title>Top 10 Praias da Bahia</v-card-title>
+            </v-img>
+            <v-card-subtitle class="pt-3">Salvador</v-card-subtitle>
+            <v-card-text>
+              <div>Rio vermelho</div>
+
+              <div>Lorem, ipsum dolor sit</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn>Ver mais</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <!---CARD 3-->
+        <v-col cols="12" md="4" lg="3">
+          <v-card flat class="border">
+            <v-img class="align-end text-white"
+              src="https://images.pexels.com/photos/3700900/pexels-photo-3700900.jpeg">
+              <v-card-title>Top 10 Praias da Bahia</v-card-title>
+            </v-img>
+            <v-card-subtitle class="pt-3">Salvador</v-card-subtitle>
+            <v-card-text>
+              <div>Rio vermelho</div>
+
+              <div>Lorem, ipsum dolor sit</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn>Ver mais</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
